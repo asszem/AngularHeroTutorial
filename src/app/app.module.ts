67a1d092016ff2a40f0ea0a6369e7035432cac96
András <asszem78@gmail.com>
2018-05-05 11:10:52 +0200

    Complete Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { FormsModule } from '@angular/forms'; // for NgModel
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroSandboxComponent } from './hero-sandbox/hero-sandbox.component'; // imported by the CLI automatically
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
@@ -19,7 +18,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    HeroSandboxComponent,
     MessagesComponent,
     DashboardComponent
   ],
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,13 +14,17 @@ export class HeroService {
   // Asynchronous signature, that returns a mock hero as an Observable, using RxJS of() function
   getHeroesFromService(): Observable<Hero[]> { // return type is an Observable for Hero[] array
     // TODO send the message AFTER fetching heroes
-    this.messageService.add('HeroService: fetched all heroes');
+    const messageText = 'HeroService: fetched all heroes';
+    this.messageService.add(messageText);
+    console.log(messageText);
     return of(HEROES); // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
   }
 
-  getASpecificHero(id: number): Observable<Hero> { // return type is a single Hero object
+  getASpecificHero(id: number): Observable<Hero> { // return type is an Observable of a single Hero object
     // TODO send the message AFTER fetching heroes
-    this.messageService.add(`HeroService: served a specific hero with ID: ${id}`); // using alt-7 backtip for js template literal
+    const messageText = `HeroService: served a specific hero with ID: ${id}`;
+    this.messageService.add(messageText); // using alt-7 backtip for js template literal
+    console.log(messageText);
     return of(HEROES.find(currentHero => currentHero.id === id));
   }
 }
