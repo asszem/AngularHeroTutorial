87e26534405414d07b395c4e4a513178cd694163
Andras Olah <andras.antal.olah@hu.ibm.com>
2018-06-14 12:34:46 +0200

    Upgrade Anguler CLI to version 6.0.8

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -17,7 +17,7 @@ import { HeroService } from '../hero.service';
 })
 export class HeroSearchComponent implements OnInit {
   heroes$: Observable<Hero[]>;
-  private searchTerms = new Subject<string>();
+  private searchTerms = new Subject<string>(); // Subject is an observable and observer
   private searchType: string;
 
   constructor(private heroService: HeroService) { }
